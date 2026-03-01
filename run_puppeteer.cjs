4c4a820049bda0606587e79570d80810c9f9cf87
Armando Blanquicet <aablanquicet27@users.noreply.github.com>
2026-03-01 12:00:30 +0000

    chore: remove venv and temp files from repo, add to gitignore

--- run_puppeteer.cjs
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-  const page = await browser.newPage();
-  await page.goto('https://html.duckduckgo.com/html/?q=Alexander+Florez+maltrato+expareja+Semana');
-  const html = await page.content();
-  console.log(html.substring(0, 500));
-  await browser.close();
-})();
